c2e1916ad374634b4745bfe77a86ee00386efd7f
venkat0086 <venkygowdru99@gmail.com>
2023-03-30 11:41:59 +0530

    ChatGPT Updated

--- chatbot-gpt/backend/index.js
+++ chatbot-gpt/backend/index.js
@@ -2,28 +2,46 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const connect = require("./src/config/db");
+// const connect = require("./src/config/db");
 
 const userController = require("./src/controllers/User_controller");
 // const studentListController = require("./src/controllers/Student_controller");
 // const eventListController = require("./src/controllers/Event_controller");
 const { register, login } = require("./src/controllers/Auth_controller");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
 app.post("/register", register);
 app.post("/login", login);
 app.use("/users", userController);
 
-const port = process.env.PORT || 8080;
+// app.listen(port, async (req, res) => {
+//   try {
+//     await connect();
+//     console.log(`Listening to ${port}`);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
-app.listen(port, async (req, res) => {
-  try {
-    await connect();
-    console.log(`Listening to ${port}`);
-  } catch (error) {
-    console.log(error);
-  }
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
 });
