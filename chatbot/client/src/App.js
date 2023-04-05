a06d6d0cd5cd2e12effb555d28837d42c63c7c76
venkat0086 <venkygowdru99@gmail.com>
2023-04-05 16:29:43 +0530

    Chatbot updated

--- chatbot/client/src/App.js
+++ chatbot/client/src/App.js
@@ -1,15 +1,15 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import Main from "./components/Main";
+import Chat from "./components/Chat";
 import Signup from "./components/Signup";
-import Login from "./components/Login";
+import Login from "./components/Signin";
 
 function App() {
   const user = localStorage.getItem("token");
 
   return (
     <Routes>
-      {user && <Route path="/" exact element={<Main />} />}
-      <Route path="/signup" exact element={<Signup />} />
+      {user && <Route path="/" exact element={<Chat />} />}
+      <Route path="/register" exact element={<Signup />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/" element={<Navigate replace to="/login" />} />
     </Routes>
---
+++ chatbot/client/src/components/Navbar.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0">
+            <Link to="/" className="text-white text-2xl font-bold">
+              Chatbot
+            </Link>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-4 flex items-center md:ml-6">
+              <button
+                onClick={handleLogout}
+                className="bg-gray-700 text-white rounded-lg py-2 px-4"
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={handleMenuToggle}
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+            >
+              <svg
+                className={`${isMenuOpen ? "hidden" : "block"} h-6 w-6`}
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              <svg
+                className={`${isMenuOpen ? "block" : "hidden"} h-6 w-6`}
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`${
+          isMenuOpen ? "block" : "hidden"
+        } md:hidden bg-gray-700 px-2 py-2 flex items-end justify-end`}
+      >
+        <button
+          onClick={handleLogout}
+          className="block text-gray-300 hover:text-white font-bold"
+        >
+          Logout
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
